e525ae28a7669dd8395d2789d1fe154144ae1417
tonywanggit <tonywang_efun@hotmail.com>
2014-08-19 14:28:01 +0800

    增加对调用中心的Ajax封�

---
+++ Core/CoreWeb/Script/mb-esb-1.0.0.js
@@ -0,0 +1,34 @@
+﻿/*
+ *  mb-esb.js 
+ *  Tony 2014.8.19
+ *  ESB AJAX调用类
+ */
+
+jQuery.esb = {
+    /*
+     * 版本号
+     */
+    version: '1.0.0',
+    /*
+     * 调用中心地址
+     */
+    baseurl: 'http://192.168.56.1/CallCenter/',
+    /*
+     *  请求响应调用
+     */
+    invoke: function (serviceName, methodName, message, fn) {
+        var response;
+        var invokeUrl = jQuery.esb.baseurl + "ESB_InvokeService.ashx?callback=?";
+        $.ajax({
+            url: invokeUrl,
+            async: false,
+            crossDomain: true,
+            dataType: "jsonp",
+            data: "ServiceName=" + serviceName + "&MethodName=" + methodName + "&Message=" + message,
+            success: function (msg) {
+                response = msg.message;
+                fn(response);
+            }
+        });
+    }
+}
