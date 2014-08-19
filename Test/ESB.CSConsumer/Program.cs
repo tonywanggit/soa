7c21a0c6e020e5fd1054b7441790449517db0f50
tonywanggit <tonywang_efun@hotmail.com>
2014-08-19 17:00:36 +0800

    增加CS Consumer客户端

---
+++ Test/ESB.CSConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using ESB.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ESB.CSConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            ESBProxy esbProxy = ESBProxy.GetInstance();
+            stopWatch.Stop();
+
+            Console.WriteLine("ESBProxy Init 耗时：{0}ms。", stopWatch.ElapsedMilliseconds);
+            Console.ReadKey();
+
+            stopWatch.Restart();
+            String message = esbProxy.Invoke("WXSC_WeiXinServiceForApp", "GET:XML:CollocationDetailFilter", "collocationId=11");
+            stopWatch.Stop();
+
+            Console.WriteLine("第1次调用 耗时：{0}ms。", stopWatch.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
