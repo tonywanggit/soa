80edc062846d67a7113383beee67c27ffd2f80c1
tonywanggit <tonywang_efun@hotmail.com>
2014-10-13 00:29:58 +0800

    增加WCF服务测试

---
+++ Test/ESB.WCF.DemoService/Demo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Activation;
+using System.Text;
+
+namespace ESB.WCF.DemoService
+{
+    [AspNetCompatibilityRequirements(RequirementsMode=AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Demo : IDemo
+    {
+        public string Echo(string message)
+        {
+            return "Echo:" + message;
+        }
+    }
+}
---
+++ Test/ESB.WCF.IDemo/IDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace ESB.WCF
+{
+    [ServiceContract]
+    public interface IDemo
+    {
+        [OperationContract]
+        String Echo(String message);
+    }
+}
